ef6c6bb726a36497b7212feabb530ce981457711
Lucasvieirar <lucasvieiraribeiro2055@gmail.com>
2026-04-19 22:19:25 +0000

    cria models do product

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from . import models
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'brand', 'price',)
+    search_fields = ('title',)
+    list_filt = ('category', 'brand',)
 
-# Register your models here.
+admin.site.register(models.Product, ProductAdmin)
