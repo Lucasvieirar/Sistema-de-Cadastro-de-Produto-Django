a600ed2c685671e9aea61d8920b140c65ad488a1
Lucasvieirar <lucasvieiraribeiro2055@gmail.com>
2026-04-19 22:30:03 +0000

    crie views e hello world

--- app/urls.py
+++ app/urls.py
@@ -17,7 +17,10 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path
+from products.views import product_view
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+
+    path("products/", product_view),
 ]
